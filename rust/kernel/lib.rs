219882e2a3ac67baca86eaff066aca4706abc1f9
Andreas Hindborg <a.hindborg@samsung.com>
2023-04-29 17:25:11 +0200

    IM: add `module_params` macro
    This patch includes changes required for Rust kernel modules to utilize module
    parameters.
    
    Imported with minor changes from `rust` branch [1], see original code for
    attributions.
    
    [1] https://github.com/Rust-for-Linux/linux/tree/bc22545f38d74473cfef3e9fd65432733435b79f
    
    Cc: adamrk <ark.email@gmail.com>
    
    ---
    
    Required by rnull module

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -17,6 +17,7 @@
 #![feature(new_uninit)]
 #![feature(receiver_trait)]
 #![feature(unsize)]
+#![feature(const_mut_refs)]
 
 // Ensure conditional compilation based on the kernel configuration works;
 // otherwise we may silently break things like initcall handling.
@@ -39,6 +40,8 @@ pub mod ioctl;
 #[cfg(CONFIG_KUNIT)]
 pub mod kunit;
 pub mod list;
+#[doc(hidden)]
+pub mod module_param;
 #[cfg(CONFIG_NET)]
 pub mod net;
 pub mod page;
