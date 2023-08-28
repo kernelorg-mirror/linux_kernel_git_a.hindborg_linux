ff14fb9caf4aa9b73d298889acc9dccdbf330930
Andreas Hindborg <a.hindborg@samsung.com>
2023-08-28 16:08:10 +0200

    rust: add irq abstractions
    Based on irq.rs from https://github.com/Rust-for-Linux/linux/commit/575b1c015f5fdaac4b8b85ba3d579c324c3c2f41
    
    Signed-off-by: Philipp Stanner <pstanner@redhat.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -19,6 +19,9 @@
 #include <linux/file.h>
 #include <linux/firmware.h>
 #include <linux/fs.h>
+#include <linux/io.h>
+#include <linux/interrupt.h>
+#include <linux/irq.h>
 #include <linux/jiffies.h>
 #include <linux/jump_label.h>
 #include <linux/mdio.h>
--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -51,6 +51,7 @@ pub mod firmware;
 pub mod fs;
 pub mod init;
 pub mod ioctl;
+pub mod irq;
 pub mod jump_label;
 #[cfg(CONFIG_KUNIT)]
 pub mod kunit;
