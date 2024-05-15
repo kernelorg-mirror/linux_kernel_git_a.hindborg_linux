fc66cf81382647c19d7e8c04b0b5a61ec13c8dfb
Andreas Hindborg <a.hindborg@samsung.com>
2024-05-15 13:47:07 -0600

    rust: block: add `Request` private data support
    This patch allows users of the Rust block device driver API to attach private
    data to the `Request` structure.

--- rust/kernel/block/mq.rs
+++ rust/kernel/block/mq.rs
@@ -69,7 +69,13 @@
 //!
 //! #[vtable]
 //! impl Operations for MyBlkDevice {
+//!     type RequestData = ();
 //!     type QueueData = ();
+//!
+//!     fn new_request_data(
+//!     ) -> impl PinInit<()> {
+//!         kernel::init::zeroed()
+//!     }
 //!
 //!     fn queue_rq(_queue_data: (), rq: ARef<Request<Self>>, _is_last: bool) -> Result {
 //!         Request::end_ok(rq);
