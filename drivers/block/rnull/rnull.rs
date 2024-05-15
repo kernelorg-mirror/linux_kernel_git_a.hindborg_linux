c587849f6f439b6ff266bab66e907f818650eed1
Andreas Hindborg <a.hindborg@samsung.com>
2024-05-15 16:45:13 -0600

    block: rust: add `hctx` private data support
    C block device drivers can attach private data to a hardware context
    (`struct blk_mq_hw_ctx`). Add support for this feature for Rust block
    device drivers via the `Operations::HwData` associated type.
    
    The private data is created in the `init_hctx` callback and stored in
    the `driver_data` field of `blk_mq_hw_ctx`. It is passed to `queue_rq`,
    `commit_rqs`, and `poll` callbacks, and is released in `exit_hctx`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -534,6 +534,7 @@ impl Operations for NullBlkDevice {
     type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
     type TagSetData = ();
+    type HwData = ();
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
         pin_init!(Pdu {
@@ -544,6 +545,7 @@ impl Operations for NullBlkDevice {
 
     #[inline(always)]
     fn queue_rq(
+        _hw_data: (),
         queue_data: Pin<&QueueData>,
         mut rq: Owned<mq::Request<Self>>,
         _is_last: bool,
@@ -575,7 +577,11 @@ impl Operations for NullBlkDevice {
         Ok(())
     }
 
-    fn commit_rqs(_queue_data: Pin<&QueueData>) {}
+    fn commit_rqs(_hw_data: (), _queue_data: Pin<&QueueData>) {}
+
+    fn init_hctx(_tagset_data: (), _hctx_idx: u32) -> Result {
+        Ok(())
+    }
 
     fn complete(rq: ARef<mq::Request<Self>>) {
         Self::end_request(
