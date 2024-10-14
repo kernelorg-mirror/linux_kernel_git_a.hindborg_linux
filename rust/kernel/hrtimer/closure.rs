616669cdf0dd9783ad5e315acdaa552386a211dd
Andreas Hindborg <a.hindborg@kernel.org>
2024-10-14 15:59:50 +0200

    rust: hrtimer: add `TimerMode`
    Allow selection of timer mode by passing a `TimerMode` variant to
    `Timer::new`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/hrtimer/closure.rs
+++ rust/kernel/hrtimer/closure.rs
@@ -50,7 +50,7 @@ where
         Box::pin_init(
             pin_init!(
                 Self {
-                    timer <- Timer::new(),
+                    timer <- Timer::new(super::TimerMode::Relative),
                     callback <- new_mutex!(Some(f)),
                 }
             ),
