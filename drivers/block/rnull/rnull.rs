c6e07075a725c0d23c234e69be45036290795804
Andreas Hindborg <a.hindborg@kernel.org>
2025-02-10 14:06:44 +0100

    block: rnull: add submit queue count config option
    Allow user space to control number of submission queues when creating null block
    devices.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -65,8 +65,9 @@ impl NullBlkDevice {
         irq_mode: IRQMode,
         completion_time: Ktime,
         memory_backed: bool,
+        submit_queues: u32,
     ) -> Result<GenDisk<Self>> {
-        let tagset = Arc::pin_init(TagSet::new(1, 256, 1), flags::GFP_KERNEL)?;
+        let tagset = Arc::pin_init(TagSet::new(submit_queues, 256, 1), flags::GFP_KERNEL)?;
 
         let queue_data = Box::pin_init(
             pin_init!(
