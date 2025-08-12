omgold/linux

block: rnull: allow specifying the home numa node
This patch allows users to specify the numa node that rnull should use when
allocating the the tag set list and the cpu map structures.

drivers/block/rnull/configfs.rs
@@ -1,6 +1,7 @@
 use super::{NullBlkDevice, THIS_MODULE};
 use core::fmt::{Display, Write};
 use kernel::{
+    bindings,
     block::mq::gen_disk::{GenDisk, GenDiskBuilder},
     c_str,
     configfs::{self, AttributeOperations},
@@ -67,6 +68,7 @@ impl configfs::GroupOperations for Config {
                 memory_backed: 6,
                 submit_queues: 7,
                 use_per_node_hctx: 8,
+                home_node: 9,
             ],
         };
 
@@ -86,6 +88,7 @@ impl configfs::GroupOperations for Config {
                     name: name.try_into()?,
                     memory_backed: false,
                     submit_queues: 1,
+                    home_node: bindings::NUMA_NO_NODE,
                 }),
             }),
         ))
@@ -141,6 +144,7 @@ struct DeviceConfigInner {
     disk: Option<GenDisk<NullBlkDevice>>,
     memory_backed: bool,
     submit_queues: u32,
+    home_node: i32,
 }
 
 #[vtable]
@@ -178,6 +182,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.completion_time,
                 guard.memory_backed,
                 guard.submit_queues,
+                guard.home_node,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -414,6 +419,35 @@ impl configfs::AttributeOperations<8> for DeviceConfig {
             this.data.lock().submit_queues *= kernel::num_online_nodes();
         }
 
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<9> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::BufferWriter::new(page)?;
+        writer.write_fmt(fmt!("{}\n", this.data.lock().home_node))?;
+        Ok(writer.pos())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        if this.data.lock().powered {
+            return Err(EBUSY);
+        }
+
+        let text = core::str::from_utf8(page)?.trim();
+        let value = text
+            .parse::<i32>()
+            .map_err(|_| kernel::error::code::EINVAL)?;
+
+        if value == 0 || value >= kernel::num_online_nodes().try_into()? {
+            return Err(kernel::error::code::EINVAL);
+        }
+
+        this.data.lock().home_node = value;
         Ok(())
     }
 }

drivers/block/rnull/rnull.rs
@@ -18,7 +18,7 @@ use kernel::{
             Operations, TagSet,
         },
     },
-    error::Result,
+    error::{code, Result},
     new_mutex, new_spinlock,
     page::Page,
     pr_info,
@@ -77,6 +77,10 @@ module! {
             default: 0,
             description:  "Use per-node allocation for hardware context queues, 0-false, 1-true. Default: 0-false",
         },
+        home_node: i32 {
+            default: -1,
+            description: "Home node for the device. Default: -1 (no node)",
+        },
     },
 }
 
@@ -114,6 +118,7 @@ impl kernel::InPlaceModule for NullBlkModule {
                     Ktime::from_nanos(completion_time),
                     *module_parameters::memory_backed.get() != 0,
                     submit_queues,
+                    *module_parameters::home_node.get(),
                 )?;
                 disks.push(disk, flags::GFP_KERNEL)?;
             }
@@ -140,9 +145,14 @@ impl NullBlkDevice {
         completion_time: Ktime,
         memory_backed: bool,
         submit_queues: u32,
+        home_node: i32,
     ) -> Result<GenDisk<Self>> {
+        if home_node > kernel::num_online_nodes().try_into()? {
+            return Err(code::EINVAL);
+        }
+
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, bindings::NUMA_NO_NODE),
+            TagSet::new(submit_queues, 256, 1, home_node),
             flags::GFP_KERNEL,
         )?;
 

kernelorg-mirror/linux_kernel_git_a.hindborg_linux

drivers/block/rnull/configfs.rs
@@ -3,6 +3,7 @@
 use super::{NullBlkDevice, THIS_MODULE};
 use core::fmt::{Display, Write};
 use kernel::{
+    bindings,
     block::mq::gen_disk::{GenDisk, GenDiskBuilder},
     c_str,
     configfs::{self, AttributeOperations},
@@ -67,6 +68,7 @@ impl configfs::GroupOperations for Config {
                 memory_backed: 6,
                 submit_queues: 7,
                 use_per_node_hctx: 8,
+                home_node: 9,
             ],
         };
 
@@ -86,6 +88,7 @@ impl configfs::GroupOperations for Config {
                     name: name.try_into()?,
                     memory_backed: false,
                     submit_queues: 1,
+                    home_node: bindings::NUMA_NO_NODE,
                 }),
             }),
         ))
@@ -141,6 +144,7 @@ struct DeviceConfigInner {
     disk: Option<GenDisk<NullBlkDevice>>,
     memory_backed: bool,
     submit_queues: u32,
+    home_node: i32,
 }
 
 #[vtable]
@@ -178,6 +182,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.completion_time,
                 guard.memory_backed,
                 guard.submit_queues,
+                guard.home_node,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -414,6 +419,35 @@ impl configfs::AttributeOperations<8> for DeviceConfig {
             this.data.lock().submit_queues *= kernel::num_online_nodes();
         }
 
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<9> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::Formatter::new(page);
+        writer.write_fmt(fmt!("{}\n", this.data.lock().home_node))?;
+        Ok(writer.bytes_written())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        if this.data.lock().powered {
+            return Err(EBUSY);
+        }
+
+        let text = core::str::from_utf8(page)?.trim();
+        let value = text
+            .parse::<i32>()
+            .map_err(|_| kernel::error::code::EINVAL)?;
+
+        if value == 0 || value >= kernel::num_online_nodes().try_into()? {
+            return Err(kernel::error::code::EINVAL);
+        }
+
+        this.data.lock().home_node = value;
         Ok(())
     }
 }

drivers/block/rnull/rnull.rs
@@ -18,7 +18,7 @@ use kernel::{
             Operations, TagSet,
         },
     },
-    error::Result,
+    error::{code, Result},
     new_mutex, new_spinlock,
     page::Page,
     prelude::*,
@@ -74,6 +74,10 @@ module! {
             default: 0,
             description:  "Use per-node allocation for hardware context queues, 0-false, 1-true. Default: 0-false",
         },
+        home_node: i32 {
+            default: -1,
+            description: "Home node for the device. Default: -1 (no node)",
+        },
     },
 }
 
@@ -111,6 +115,7 @@ impl kernel::InPlaceModule for NullBlkModule {
                     Ktime::from_nanos(completion_time),
                     *module_parameters::memory_backed.value() != 0,
                     submit_queues,
+                    *module_parameters::home_node.value(),
                 )?;
                 disks.push(disk, flags::GFP_KERNEL)?;
             }
@@ -137,9 +142,14 @@ impl NullBlkDevice {
         completion_time: Ktime,
         memory_backed: bool,
         submit_queues: u32,
+        home_node: i32,
     ) -> Result<GenDisk<Self>> {
+        if home_node > kernel::num_online_nodes().try_into()? {
+            return Err(code::EINVAL);
+        }
+
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, bindings::NUMA_NO_NODE),
+            TagSet::new(submit_queues, 256, 1, home_node),
             flags::GFP_KERNEL,
         )?;
 