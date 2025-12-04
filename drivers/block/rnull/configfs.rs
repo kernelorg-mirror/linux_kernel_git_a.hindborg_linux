omgold/linux

block: rnull: add submit queue count config option
Allow user space to control number of submission queues when creating null block
devices.

Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

drivers/block/rnull/configfs.rs
@@ -36,7 +36,10 @@ impl AttributeOperations<0> for Config {
 
     fn show(_this: &Config, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
         let mut writer = kernel::str::BufferWriter::new(page)?;
-        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec,memory_backed\n")?;
+        writer.write_str(
+            "blocksize,size,rotational,irqmode,completion_nsec,memory_backed\
+             submit_queues\n",
+        )?;
         Ok(writer.pos())
     }
 }
@@ -62,6 +65,7 @@ impl configfs::GroupOperations for Config {
                 irqmode: 4,
                 completion_nsec: 5,
                 memory_backed: 6,
+                submit_queues: 7,
             ],
         };
 
@@ -80,6 +84,7 @@ impl configfs::GroupOperations for Config {
                     completion_time: Ktime::from_nanos(0),
                     name: name.try_into()?,
                     memory_backed: false,
+                    submit_queues: 1,
                 }),
             }),
         ))
@@ -134,6 +139,7 @@ struct DeviceConfigInner {
     completion_time: Ktime,
     disk: Option<GenDisk<NullBlkDevice>>,
     memory_backed: bool,
+    submit_queues: u32,
 }
 
 #[vtable]
@@ -170,6 +176,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.irq_mode,
                 guard.completion_time,
                 guard.memory_backed,
+                guard.submit_queues,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -342,6 +349,35 @@ impl configfs::AttributeOperations<6> for DeviceConfig {
             .map_err(|_| kernel::error::code::EINVAL)?
             != 0;
 
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<7> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::BufferWriter::new(page)?;
+        writer.write_fmt(fmt!("{}\n", this.data.lock().submit_queues))?;
+        Ok(writer.pos())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        if this.data.lock().powered {
+            return Err(EBUSY);
+        }
+
+        let text = core::str::from_utf8(page)?.trim();
+        let value = text
+            .parse::<u32>()
+            .map_err(|_| kernel::error::code::EINVAL)?;
+
+        if value == 0 || value > kernel::num_possible_cpus() {
+            return Err(kernel::error::code::EINVAL);
+        }
+
+        this.data.lock().submit_queues = value;
         Ok(())
     }
 }

kernelorg-mirror/linux_kernel_git_a.hindborg_linux

drivers/block/rnull/configfs.rs
@@ -37,7 +37,10 @@ impl AttributeOperations<0> for Config {
 
     fn show(_this: &Config, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
         let mut writer = kernel::str::Formatter::new(page);
-        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec,memory_backed\n")?;
+        writer.write_str(
+            "blocksize,size,rotational,irqmode,completion_nsec,memory_backed\
+             submit_queues\n",
+        )?;
         Ok(writer.bytes_written())
     }
 }
@@ -62,6 +65,7 @@ impl configfs::GroupOperations for Config {
                 irqmode: 4,
                 completion_nsec: 5,
                 memory_backed: 6,
+                submit_queues: 7,
             ],
         };
 
@@ -80,6 +84,7 @@ impl configfs::GroupOperations for Config {
                     completion_time: time::Delta::ZERO,
                     name: name.try_into()?,
                     memory_backed: false,
+                    submit_queues: 1,
                 }),
             }),
         ))
@@ -134,6 +139,7 @@ struct DeviceConfigInner {
     completion_time: time::Delta,
     disk: Option<GenDisk<NullBlkDevice>>,
     memory_backed: bool,
+    submit_queues: u32,
 }
 
 #[vtable]
@@ -165,6 +171,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.irq_mode,
                 guard.completion_time,
                 guard.memory_backed,
+                guard.submit_queues,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -327,6 +334,35 @@ impl configfs::AttributeOperations<6> for DeviceConfig {
             .map_err(|_| kernel::error::code::EINVAL)?
             != 0;
 
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<7> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::Formatter::new(page);
+        writer.write_fmt(fmt!("{}\n", this.data.lock().submit_queues))?;
+        Ok(writer.bytes_written())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        if this.data.lock().powered {
+            return Err(EBUSY);
+        }
+
+        let text = core::str::from_utf8(page)?.trim();
+        let value = text
+            .parse::<u32>()
+            .map_err(|_| kernel::error::code::EINVAL)?;
+
+        if value == 0 || value > kernel::num_possible_cpus() {
+            return Err(kernel::error::code::EINVAL);
+        }
+
+        this.data.lock().submit_queues = value;
         Ok(())
     }
 }