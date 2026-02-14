751eb275de7f44f99b519ad156298e45abfb5272
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-14 15:22:49 +0100

    block: rust: add a method to get the request queue for a request
    Add a method to `Request` for obtaining the associated `RequestQueue`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/block/mq/request.rs
+++ rust/kernel/block/mq/request.rs
@@ -39,6 +39,7 @@ use core::{
     ptr::NonNull, //
 };
 
+use super::RequestQueue;
 use crate::block::bio::{
     Bio,
     BioIterator, //
@@ -139,6 +140,12 @@ impl<T: Operations> RequestInner<T> {
     pub fn queue_data(&self) -> <T::QueueData as ForeignOwnable>::Borrowed<'_> {
         // SAFETY: By type invariants of `Request`, `self.0` is a valid request.
         unsafe { T::QueueData::borrow((*(*self.0.get()).q).queuedata) }
+    }
+
+    /// Get the request queue associated with this request.
+    pub fn queue(&self) -> &RequestQueue<T> {
+        // SAFETY: By type invariant, self.0 is guaranteed to be valid.
+        unsafe { RequestQueue::from_raw((*self.0.get()).q) }
     }
 }
 
