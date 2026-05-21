50855eed129be7a241bbb8d19c6ad124f1b0be47
Alvin Sun <alvin.sun@linux.dev>
2026-05-21 14:57:12 +0800

    rust: block: rnull: use vertical import style
    Convert `use` imports to vertical layout for better readability and
    maintainability.
    
    Signed-off-by: Alvin Sun <alvin.sun@linux.dev>
    Link: https://msgid.link/20260521-miscdev-use-format-v3-6-56240ca70d0c@linux.dev
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/configfs.rs
+++ drivers/block/rnull/configfs.rs
@@ -2,15 +2,27 @@
 
 use super::NullBlkDevice;
 use kernel::{
-    block::mq::gen_disk::{GenDisk, GenDiskBuilder},
-    configfs::{self, AttributeOperations},
+    block::mq::gen_disk::{
+        GenDisk,
+        GenDiskBuilder, //
+    },
+    configfs::{
+        self,
+        AttributeOperations, //
+    },
     configfs_attrs,
-    fmt::{self, Write as _},
+    fmt::{
+        self,
+        Write as _, //
+    },
     new_mutex,
     page::PAGE_SIZE,
     prelude::*,
-    str::{kstrtobool_bytes, CString},
-    sync::Mutex,
+    str::{
+        kstrtobool_bytes,
+        CString, //
+    },
+    sync::Mutex, //
 };
 
 pub(crate) fn subsystem() -> impl PinInit<kernel::configfs::Subsystem<Config>, Error> {
