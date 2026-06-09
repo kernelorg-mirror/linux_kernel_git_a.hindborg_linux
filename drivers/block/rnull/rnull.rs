Engineer-HaMa/linux

block: rust: add `TagSet` private data support
C block device drivers can attach private data to a `struct
blk_mq_tag_set`. Add support for this feature for Rust block device
drivers via the `Operations::TagSetData` associated type.

The private data is passed to `TagSet::new` and is stored in the
`driver_data` field of the underlying `struct blk_mq_tag_set`. It is
released when the `TagSet` is dropped.

Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

drivers/block/rnull/rnull.rs
@@ -225,7 +225,7 @@ impl NullBlkDevice {
         }
 
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, home_node, flags),
+            TagSet::new(submit_queues, (), 256, 1, home_node, flags),
             GFP_KERNEL,
         )?;
 
@@ -501,6 +501,7 @@ where
 impl Operations for NullBlkDevice {
     type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
+    type TagSetData = ();
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
         pin_init!(Pdu {

kernelorg-mirror/linux_kernel_git_a.hindborg_linux

drivers/block/rnull/rnull.rs
@@ -240,7 +240,7 @@ impl NullBlkDevice {
         }
 
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, numa_node, flags),
+            TagSet::new(submit_queues, (), 256, 1, numa_node, flags),
             GFP_KERNEL,
         )?;
 
@@ -533,6 +533,7 @@ where
 impl Operations for NullBlkDevice {
     type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
+    type TagSetData = ();
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
         pin_init!(Pdu {